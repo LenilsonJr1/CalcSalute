6be7c5575a8503d14f96283fbfd9bda1c7d578da
KalebLight <kaleb.luiz00@gmail.com>
2021-09-23 16:33:33 -0300

    tela login

---
+++ Android Nativo/CalcSalute/app/src/main/java/com/example/calcsalute/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.example.calcsalute;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText altura;
+    private EditText peso;
+    private Button botao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+
+
+
+
+
+
+    }
+}
