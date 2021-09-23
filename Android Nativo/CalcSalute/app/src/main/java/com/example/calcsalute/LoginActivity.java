367c5b7bc3649cd221d454f87a1d256cb220c3c8
KalebLight <kaleb.luiz00@gmail.com>
2021-09-23 18:23:21 -0300

    tela de historico, menu e ajustes

--- Android Nativo/CalcSalute/app/src/main/java/com/example/calcsalute/LoginActivity.java
+++ Android Nativo/CalcSalute/app/src/main/java/com/example/calcsalute/LoginActivity.java
@@ -9,14 +9,13 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
-    private EditText altura;
-    private EditText peso;
-    private Button botao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
 
 
 
---
+++ Android Nativo/CalcSalute/app/src/main/java/com/example/calcsalute/MenuActivity.java
@@ -0,0 +1,26 @@
+package com.example.calcsalute;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
